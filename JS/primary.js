9d5d394cc751d5c7a6357fb0fc4f006eb13b1040
Matt Tilley <mtilley255@gmail.com>
2018-07-15 09:47:06 -0500

    Been trying various things / snapshot of where I'm at.

--- JS/primary.js
+++ JS/primary.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function(){
 
 
@@ -7,23 +9,93 @@ $(document).ready(function(){
 
       console.log(type);
      
-
     });
 
 
+    /* $("#sowDate").datepicker().on("change", function(){
+
+        console.log( $("#sowDate").datepicker("getDate"));
+    }); */
+
+
+    /* $("#sowDate").datepicker().on("change", function(){
+
+        let x =  $("#sowDate").datepicker("getDate");
+
+        console.log(x + "OIJASOIJASD");
+    }); */
+    
+    
+    /* $("#sowDate").datepicker().on("change", function(){
+
+        return $("#sowDate").datepicker("getDate");
+    });
+
+    console.log(x.val()); */
+
+    /* function getDate(){
+
+        x = $("#sowDate").datepicker("getDate");
+        
+        return x;
+    }
+
+
+    let startDate = getDate(x);
+
+    console.log(startDate); */
+
+
+
+    /* $("#sowDate").datepicker().on("change", function(){
+
+        let x =  $("#sowDate").datepicker("getDate");
+
+        console.log(x + "OIJASOIJASD");
+    }); */
+
+
+
+
+
+   /*  
+   THIS ONE WORKS
+   let getDate = $("#sowDate").datepicker().on("change", function(){
+
+            let x = $("#sowDate").datepicker("getDate").getTime();
+
+            return x;
+    }); */
 
     $("#sowDate").datepicker();
 
-    $("#sowDate").on("change", function(){
-        console.log(Math.random() + 1);
+    
+    let startDate = $("#sowDate").on("change", function(){
+
+        let x = $("#sowDate").datepicker("getDate").getTime();
+
+        
+
+       return x;
+
+        
+
     });
 
 
+    console.log(startDate());
+
+ /*    var time = new Date().getTime();
+    var date = new Date(time);
+    alert(date.toString()); // Wed Jan 12 2011 12:42:46 GMT-0800 (PST)
     
 
-    $/* (".DDBX-T-01").selectmenu(); */
-
-
+ */
 
 
 });
+
+
+
+
+
