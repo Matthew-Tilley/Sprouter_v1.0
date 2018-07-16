3b4968233b30633dee920e2ef638d4699fce7467
Matt Tilley <mtilley255@gmail.com>
2018-07-16 08:06:26 -0500

    Figured out problem with #seedPicker dropdown / now working again.

--- JS/primary.js
+++ JS/primary.js
@@ -1,101 +1,16 @@
-
-
 $(document).ready(function(){
-
-
+    
+   /* $("#seedPicker").selectmenu();
+    $("#sowDate").datepicker();*/
+    
+    
     $("#seedPicker").on("change", function(){
         
-      let type = $(this).val();
-
-      console.log(type);
-     
-    });
-
-
-    /* $("#sowDate").datepicker().on("change", function(){
-
-        console.log( $("#sowDate").datepicker("getDate"));
-    }); */
-
-
-    /* $("#sowDate").datepicker().on("change", function(){
-
-        let x =  $("#sowDate").datepicker("getDate");
-
-        console.log(x + "OIJASOIJASD");
-    }); */
-    
-    
-    /* $("#sowDate").datepicker().on("change", function(){
-
-        return $("#sowDate").datepicker("getDate");
-    });
-
-    console.log(x.val()); */
-
-    /* function getDate(){
-
-        x = $("#sowDate").datepicker("getDate");
+        console.log( $(this).val() );
         
-        return x;
-    }
-
-
-    let startDate = getDate(x);
-
-    console.log(startDate); */
-
-
-
-    /* $("#sowDate").datepicker().on("change", function(){
-
-        let x =  $("#sowDate").datepicker("getDate");
-
-        console.log(x + "OIJASOIJASD");
-    }); */
-
-
-
-
-
-   /*  
-   THIS ONE WORKS
-   let getDate = $("#sowDate").datepicker().on("change", function(){
-
-            let x = $("#sowDate").datepicker("getDate").getTime();
-
-            return x;
-    }); */
-
-    $("#sowDate").datepicker();
-
-    
-    let startDate = $("#sowDate").on("change", function(){
-
-        let x = $("#sowDate").datepicker("getDate").getTime();
-
-        
-
-       return x;
-
-        
-
     });
-
-
-    console.log(startDate());
-
- /*    var time = new Date().getTime();
-    var date = new Date(time);
-    alert(date.toString()); // Wed Jan 12 2011 12:42:46 GMT-0800 (PST)
     
-
- */
-
-
+    
+    
+    
 });
-
-
-
-
-
