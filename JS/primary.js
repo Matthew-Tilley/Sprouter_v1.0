c75401cd284d889ca66985f258b09048f6201158
Matt Tilley <mtilley255@gmail.com>
2018-07-18 08:55:38 -0500

    Added notes to JS file.

--- JS/primary.js
+++ JS/primary.js
@@ -1,26 +1,15 @@
 $(document).ready(function(){
     
+
+    // THIS INITIALIZES THE jQUERY-UI DATEPICKER:
     
     $("#sowDate").datepicker();
+
     
     
-    /*  THIS CODE IS WORKING PROPERLY - SAVING FOR LATER
-    
-    $("#seedPicker").on("change", function(){
-        
-        console.log( $(this).val() );
-        
-    });
-    
-    */
-    
-    
-   
-    
-   
-    
-    
-    
+    // THIS OBTAINS A DATE VALUE PER USER INPUT AND LOGS IT
+    // TO THE CONSOLE.  (AT THIS POINT I DON'T UNDERSTAND 
+    // HOW TO RETURN THE VALUE AND USE IT ELSEWHERE).
     
     $("#sowDate").datepicker().on("change", function(){
         
@@ -29,6 +18,27 @@ $(document).ready(function(){
         console.log(tempStart);
         
     });
+    
+    
+    
+    
+    $("#seedPicker").on("change", function(){
+        
+        console.log( $(this).val() );
+        
+    });
+    
+    
+    
+    
+   
+    
+   
+    
+    
+    
+    
+    
     
     
     
@@ -44,42 +54,6 @@ $(document).ready(function(){
 
 
 
-/*
-
-EXAMPLE #23
-
-Forget about all the text-field stuff for right now...just trying to get the
-return value situation squared away.
-
-So, I made a function and had it return the value of "Farenheit" after converting 
-from Celcius.  Yay...it works!
-
-What if I want to just store that variable somewhere and access / use it later in
-my code???
-
-
-*/
-
-
-let x = function convert(){
-    
-    let celcius = prompt("Enter the degrees in Celcius");
-    //I am getting a value from user via the prompt method and assigning it to variable celcius.
-    
-    
-    let farenheit = celcius * 1.8 + 32;
-    //Here I am instantiating the variable farenheit and storing a calculated result therein.
-    
-    
-    return farenheit;
-    //Now I am returning the result from above.
-    
-    
-    
-}
-
-
-console.log(x());
 
 
 
